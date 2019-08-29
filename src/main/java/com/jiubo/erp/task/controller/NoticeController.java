724b30d9423d60b41b05388629784a9972e277bb
1332165192 <1332165192@qq.com>
2019-08-29 13:49:45 +0800

    办公用品申请通知

---
+++ src/main/java/com/jiubo/erp/task/controller/NoticeController.java
@@ -0,0 +1,17 @@
+package com.jiubo.erp.task.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @desc:
+ * @date: 2019-08-28 09:15
+ * @author: dx
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("/noticeController")
+public class NoticeController {
+
+    
+}
